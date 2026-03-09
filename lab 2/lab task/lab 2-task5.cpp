6242254004eaf23543bf89cab04cc37ab63a224f
ronaqk735-dotcom <ronaqk735@gmail.com>
2026-03-09 20:20:57 +0500

    Add files via upload

---
+++ lab 2/lab task/lab 2-task5.cpp
@@ -0,0 +1,24 @@
+// Name: Ronaq hassan
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0736
+// Section: A
+
+#include <iostream>   // library for input and output
+using namespace std;  // allows using cout directly
+
+int main()
+{
+    // printing name 10 times
+    cout << "Ronaq hassan" << endl;
+    cout << "Ronaq hassan" << endl;
+    cout << "Ronaq hassan" << endl;
+    cout << "Ronaq hassan" << endl;
+    cout << "Ronaq hassan" << endl;
+    cout << "Ronaq hassan" << endl;
+    cout << "Ronaq hassan" << endl;
+    cout << "Ronaq hassan" << endl;
+    cout << "Ronaq hassan" << endl;
+    cout << "Ronaq hassan" << endl;
+
+    return 0;  // program ends
+}
