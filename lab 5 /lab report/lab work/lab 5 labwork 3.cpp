68265db6288c263098590cc0135e346371f799df
ronaqk735-dotcom <ronaqk735@gmail.com>
2026-03-12 05:47:04 +0500

    Add files via upload

---
+++ lab 5 /lab report/lab work/lab 5 labwork 3.cpp
@@ -0,0 +1,25 @@
+// Name:Ronaq hassan
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0736
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, i = 1, sum = 0;
+
+    cout << "Enter a positive integer: ";
+    cin >> num;
+
+    while(i <= num)
+    {
+        sum = sum + i;
+        i++;
+    }
+
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
