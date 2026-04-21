e8ad5af2a0f9e437e8fc74c5ee968c90f7d72c71
ronaqk735-dotcom <ronaqk735@gmail.com>
2026-04-21 09:20:06 +0500

    Add files via upload

---
+++ lab 1  /lab work/lab 1 labwork 1.cpp
@@ -0,0 +1,14 @@
+
+
+#include <iostream>   // Header file for input and output
+using namespace std;  // Using standard namespace
+
+int main() 
+{
+    // Printing Hello World on the screen
+    cout << "Hello World!";
+
+    return 0;  // Program ends here
+}
+
+
