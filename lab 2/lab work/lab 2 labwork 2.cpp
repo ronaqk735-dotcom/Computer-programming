d06ab92c43e8d1ff8ad3b1adf9440b555348703c
ronaqk735-dotcom <ronaqk735@gmail.com>
2026-04-21 09:31:54 +0500

    Add files via upload

---
+++ lab 2/lab work/lab 2 labwork 2.cpp
@@ -0,0 +1,12 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float pi = 3.14;  // declare float variable pi
+    cout << "Value of pi = " << pi << endl;  // display the value
+    return 0;
+}
+
+
---
+++ lab 2/lab work/lab 2 labwork 3.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float celsius, fahrenheit;  // declare variables
+
+    cout << "Enter temperature in Celsius: ";
+    cin >> celsius;  // take input from user
+
+    // Conversion formula
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    cout << "Temperature in Fahrenheit: " << fahrenheit << endl;
+
+    return 0;
+}
+
