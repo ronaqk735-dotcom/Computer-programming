2c36a31472fc222c0cb6908b14ac22d092fb436f
ronaqk735-dotcom <ronaqk735@gmail.com>
2026-06-26 16:08:04 +0500

    Add files via upload

---
+++ lab11/lab work/LAB11LABWORK2.cpp
@@ -0,0 +1,24 @@
+// Name: RONAQ HASSAN
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0736
+// Section: A
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Declaring and initializing first name
+    string firstName = "RONAQ ";
+
+    // Declaring and initializing last name
+    string lastName = "HASSAN";
+
+    // Concatenating first name and last name with a space in between
+    string fullName = firstName + " " + lastName;
+
+    // Displaying the full name
+    cout << "Full Name: " << fullName << endl;
+
+    return 0;
+}
